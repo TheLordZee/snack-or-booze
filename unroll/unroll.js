cf0054b11694c27ce59ed9e04f4a944e0d474416
lordzee <l0rclz33@gmail.com>
2021-10-09 16:29:26 -0400

    finished unroll

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,22 @@
 function unroll(squareArray) {
-
+    if(squareArray.length === 0){
+        return []
+     }
+     let res = squareArray.shift()
+     for(let i = 0; i < squareArray.length - 1; i++){
+       res.push(squareArray[i].pop())  
+     }
+     let temp = squareArray.pop()
+     if(temp === undefined){
+         return res
+     }
+     for(let i = temp.length-1; i >= 0; i--){
+         res.push(temp[i])
+     }
+     for(let i = (squareArray.length-1); i >= 0 ; i--){
+       res.push(squareArray[i].shift())  
+     }
+     return res.concat(unroll(squareArray))
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,39 @@
 const unroll = require("./unroll");
 
+const square = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+];
+
+const smallerSquare = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"]
+];
+
+const smallestSquare = [
+  [1,2],
+  [3,4]
+]
+
 describe("#unroll", function () {
 
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("unrolls arrays", function(){
+    expect(unroll(square)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
+    expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
+  })
+
+  it("unrolls smaller arrays", function(){
+    expect(unroll(smallestSquare)).toEqual([1,2,4,3])
+  })
+
+  it("works when an array is empty", function(){
+    expect(unroll([[1,2],[]])).toEqual([1,2])
+  })
 });
